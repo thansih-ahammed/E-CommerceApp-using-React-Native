1b3f9af1b2b1a613a493c2fceaa245881c2b0c97
thansih-ahammed <thansihahammed7356@gmail.com>
2023-07-04 17:30:08 +0530

    created | Pages

---
+++ App.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+    <View>
+      <Text>App</Text>
+    </View>
+    </NavigationContainer>
+  )
+}
