0a3039cb8450cdee624d5886adaf0d0f8c81d8c1
thansih-ahammed <thansihahammed7356@gmail.com>
2023-07-05 07:11:18 +0530

    setup | pages

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Cart from './src/Cart/Cart';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-    <View>
-      <Text>App</Text>
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen name='CartPage' component={<Cart/>}  />
+      </Stack.Navigator>
+        <View>
+          <Text>App</Text>
+        </View>
+  
     </NavigationContainer>
-  )
+  );
 }
